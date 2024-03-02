fcbfe52e0d24849e56374b23c274e65263819923
创梦流浪人 <1648785@qq.com>
2024-03-02 20:01:46 +0800

    1

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -23,81 +23,41 @@ class Index
 {
 
     /**
-     * 主页静态页面
-     * @return Html
+     * 生命科学馆 本校人员提交预约信息
      */
-    public function index(): Html
+    public function lifeInfoStudent()
     {
-        # html路径: ../view/index.html
-        return response(file_get_contents(dirname(dirname(__FILE__)).'/view/index.html'));
-    }
+        // $data['infoUname'] = Request::param('infoUname');
+        // echo Request::param('infoActivity');
+        // echo Request::param('infoClass');
+        // echo Request::param('infoCode');
+        // echo Request::param('infoPhone');
+        // echo Request::param('infoType');
+        // if (Request::param('infoTime') == "1") {
+        //     echo "14:30-15:00";
+        // } elseif (Request::param('infoTime') == "2") {
+        //     echo "15:30-16:00";
+        // } else {
+        //     echo "16:30-17:00";
+        // }
+        // echo Request::param('infoTime');
 
+        // echo Request::param('infoDate')['date'];
+        // echo Request::param('infoDate')['dayOfWeek'];
 
-    /**
-     * 获取todo list
-     * @return Json
-     */
-    public function getCount(): Json
-    {
-        try {
-            $data = (new Counters)->find(1);
-            if ($data == null) {
-                $count = 0;
-            }else {
-                $count = $data["count"];
-            }
-            $res = [
-                "code" => 0,
-                "data" =>  $count
-            ];
-            Log::write('getCount rsp: '.json_encode($res));
-            return json($res);
-        } catch (Error $e) {
-            $res = [
-                "code" => -1,
-                "data" => [],
-                "errorMsg" => ("查询计数异常" . $e->getMessage())
-            ];
-            Log::write('getCount rsp: '.json_encode($res));
-            return json($res);
-        }
-    }
-
-
-    /**
-     * 根据id查询todo数据
-     * @param $action `string` 类型，枚举值，等于 `"inc"` 时，表示计数加一；等于 `"reset"` 时，表示计数重置（清零）
-     * @return Json
-     */
-    public function updateCount()
-    {
-        echo Request::param('infoUname');
-        echo Request::param('infoActivity');
-        echo Request::param('infoClass');
-        echo Request::param('infoCode');
-        echo Request::param('infoPhone');
-        echo Request::param('infoType');
-        if(Request::param('infoTime') == "1"){
-            echo "14:30-15:00";
-        }elseif(Request::param('infoTime') == "2"){
-            echo "15:30-16:00";
+        if(Db::name('lifeInfoStudent')->insert(Request::post())){
+            echo '预约成功';
         }else{
-            echo "16:30-17:00";
+            echo "预约失败";
         }
-        echo Request::param('infoTime');
-
-        echo Request::param('infoDate')['date'];
-        echo Request::param('infoDate')['dayOfWeek'];
-
-
     }
+
     /**
      * 查询是否有票
      */
-
-    public function getTicketNum($date,$time){
-        echo "日期：".$date['date'];
-        echo "星期：".$date['dayOfWeek'];
-        echo "时间段".$time;
+    public function getTicketNum($date)
+    {
+        echo "日期：" . $date['date'];
+        echo "星期：" . $date['dayOfWeek'];
     }
 }
